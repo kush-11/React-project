70eae361ab51b8a32f8d034328338ae788eb8e7a
Kush <kush1604.be20@chitkara.edu.in>
2023-09-20 23:46:29 +0530

    second commit

---
+++ build/precache-manifest.e99689233afd905cf85620328d92a38c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "210546de3d75610ba872",
+    "url": "/static/css/main.55239067.chunk.css"
+  },
+  {
+    "revision": "210546de3d75610ba872",
+    "url": "/static/js/main.210546de.chunk.js"
+  },
+  {
+    "revision": "c0dba26f4ccf71cea450",
+    "url": "/static/js/1.c0dba26f.chunk.js"
+  },
+  {
+    "revision": "cc37d1a7098762d16738",
+    "url": "/static/js/2.cc37d1a7.chunk.js"
+  },
+  {
+    "revision": "561708c8e89dc260303a",
+    "url": "/static/js/runtime~main.561708c8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9f23f5c563d10542ab0c0ec3260f9f77",
+    "url": "/index.html"
+  }
+];
